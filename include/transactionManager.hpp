982656d09aae2cbb745e0ca877e91d834686076e
Marcello Menezes <marcello.eam@gmail.com>
2025-06-19 01:01:05 -0300

    Inicio de conexões front-back

--- include/transactionManager.hpp
+++ include/transactionManager.hpp
@@ -3,23 +3,25 @@
 
 #include "transaction.hpp"
 
-namespace smartbudget {
+namespace smartbudget
+{
 
-class TransactionManager {
-private:
-    vector<Transaction> transactions;
+    class TransactionManager
+    {
+    private:
+        vector<Transaction> transactions;
 
-public:
-    void addTransaction(const Transaction& t);
-    bool removeTransaction(size_t index);
-    void listTransactions() const;
-    vector<Transaction> filterByType(const string& type) const;
-    vector<Transaction> filterByCategory(const string& category) const;
-    vector<Transaction> filterByDateRange(const string& startDate, const string& endDate) const;
-    vector<Transaction> filterByAmountRange(double minAmount, double maxAmount) const;
-    bool updateTransaction(int index, const Transaction& newTransaction);
-    const vector<Transaction>& getTransactions() const;
-};
+    public:
+        void addTransaction(const Transaction &t);
+        bool removeTransaction(size_t index);
+        void listTransactions() const;
+        vector<Transaction> filterByType(const string &type) const;
+        vector<Transaction> filterByCategory(const string &category) const;
+        vector<Transaction> filterByDateRange(const string &startDate, const string &endDate) const;
+        vector<Transaction> filterByAmountRange(double minAmount, double maxAmount) const;
+        bool updateTransaction(int index, const Transaction &newTransaction);
+        const vector<Transaction> &getTransactions() const;
+    };
 
 }
 
