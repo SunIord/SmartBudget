ce080910dde2bf69c3ebba22f285d8eb22ffc63e
jpam <160938086+SunIord@users.noreply.github.com>
2025-07-09 10:02:21 -0300

    chore: reorganização da estrutura do projeto e atualização do CMake

--- SmartBudget/include/fileManager.hpp
+++ SmartBudget/include/fileManager.hpp
@@ -1,14 +1,20 @@
 #ifndef FILEMANAGER_HPP
 #define FILEMANAGER_HPP
 
-#include <string>
-#include <vector>
-
-class Transaction;
+#include "transaction.hpp"
 
 class FileManager {
+private:
+    static std::vector<std::string> parseCSVLine(const std::string& line);
+
 public:
+    static bool saveToFile(const std::vector<Transaction>& transactions, const std::string& filename);
     static bool loadFromFile(std::vector<Transaction>& transactions, const std::string& filename);
+    static std::string ensureCSVExtension(const std::string& filename); 
+    static bool fileExists(const std::string& filename);
+    static bool deleteFile(const std::string& filename);
+    static std::string generateUniqueFilename(const std::string& baseName); 
+    static std::vector<std::string> listCSVFiles();
 };
 
-#endif // FILEMANAGER_HPP
+#endif
--- src/build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-g++ -std=c++11 -I../include -o smartbudget main2.cpp transaction.cpp transactionManager.cpp budgetAnalyzer.cpp fileManager.cpp appUI2.cpp
